fef13a5dacf719de650c710bd4b7b52e156be14c
noidilin <linganinja.0120@gmail.com>
2026-06-20 10:22:39 +0800

    fix(infra): make sure the object is bootstrapped in correct order for argocd

--- infra/envs/dev/platform/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = module.argocd.kubernetes_namespace_v1.monitoring
-  to   = module.monitoring.kubernetes_namespace_v1.monitoring
-}
-
-moved {
-  from = module.argocd.helm_release.monitoring
-  to   = module.monitoring.helm_release.monitoring
-}
